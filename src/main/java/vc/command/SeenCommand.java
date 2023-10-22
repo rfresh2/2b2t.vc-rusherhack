209ac4dd6985ffec2ecad3eb14df81ccc6a8656a
rfresh2 <89827146+rfresh2@users.noreply.github.com>
2023-10-22 15:36:04 -0700

    no more shading

--- src/main/java/vc/command/SeenCommand.java
+++ src/main/java/vc/command/SeenCommand.java
@@ -1,37 +1,37 @@
 package vc.command;
 
 import org.rusherhack.client.api.feature.command.Command;
+import org.rusherhack.client.api.utils.ChatUtils;
 import org.rusherhack.core.command.annotations.CommandExecutor;
 import vc.api.VcApi;
 import vc.api.model.SeenResponse;
 
 import java.time.format.DateTimeFormatter;
 import java.util.Optional;
+import java.util.concurrent.ForkJoinPool;
 
 public class SeenCommand extends Command {
     final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 
     private final VcApi api;
     public SeenCommand(final VcApi api) {
-        super("SeenCommand", "Gets when a player was first and last seen on 2b2t");
+        super("seenvc", "Gets when a player was first and last seen on 2b2t");
         this.api = api;
     }
 
     @CommandExecutor
-    private void seenVc() {
-    }
-
-    @CommandExecutor
-    @CommandExecutor.Argument({"string"})
-    private String seenPlayerName(final String playerName) {
-        Optional<SeenResponse> firstSeen = this.api.getFirstSeen(playerName);
-        Optional<SeenResponse> lastSeen = this.api.getLastSeen(playerName);
-        if (firstSeen.isEmpty() && lastSeen.isEmpty()) {
-            return "Error: " + playerName + " not found!";
-        }
-        String out = "";
-        out += firstSeen.map(seenResponse -> "First seen: " + seenResponse.time().format(formatter) + "\n").orElse("");
-        out += lastSeen.map(seenResponse -> "Last seen: " + seenResponse.time().format(formatter)).orElse("");
-        return out;
+    @CommandExecutor.Argument({"playerName"})
+    private void seenPlayerName(final String playerName) {
+        ForkJoinPool.commonPool().execute(() -> {
+            Optional<SeenResponse> firstSeen = this.api.getFirstSeen(playerName);
+            Optional<SeenResponse> lastSeen = this.api.getLastSeen(playerName);
+            if (firstSeen.isEmpty() && lastSeen.isEmpty()) {
+                ChatUtils.print("Error: " + playerName + " not found!");
+            }
+            String out = "";
+            out += firstSeen.map(seenResponse -> "\nFirst seen: " + seenResponse.time().format(formatter)).orElse("");
+            out += lastSeen.map(seenResponse -> "\nLast seen: " + seenResponse.time().format(formatter)).orElse("");
+            ChatUtils.print(out);
+        });
     }
 }
